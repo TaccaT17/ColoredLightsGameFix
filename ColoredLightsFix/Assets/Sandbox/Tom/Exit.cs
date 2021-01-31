f4186bddd43c443da6f2ca5063d7edaf285e28a3
TaccaT17 <tom@crammond.name>
2021-01-31 15:14:17 -0800

    Added exit to treasure chest prefab, fixed bugs

--- ColoredLightsFix/Assets/Sandbox/Tom/Exit.cs
+++ ColoredLightsFix/Assets/Sandbox/Tom/Exit.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Exit : Interactable
+public class Exit : MonoBehaviour , Interactable
 {
     void Interactable.Interact(eInteractTypes type)
     {
