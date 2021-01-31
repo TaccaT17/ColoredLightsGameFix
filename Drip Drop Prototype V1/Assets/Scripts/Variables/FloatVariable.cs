988fbb3e8d5f6e46ffae068bb805aa93fb8a9d60
Twellvepack <78246621+Twellvepack@users.noreply.github.com>
2021-01-31 12:07:14 -0800

    Text Levels Ready

---
+++ Drip Drop Prototype V1/Assets/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatVariable", menuName = "Variables/Float", order = 1)]
+public class FloatVariable : ScriptableObject
+{
+    public float Value;
+}
